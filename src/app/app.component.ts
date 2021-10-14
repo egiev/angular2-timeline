8e02e321e7d3202f1bdb429c6110a8a616284507
reginald mabanta <reginaldventura23@gmail.com>
2021-10-14 10:54:09 +0800

    Optimize and update business logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,26 +9,26 @@ export class AppComponent {
   events: Array<{
     name: string;
     color: string;
-    start_date: string | Date;
-    end_date: string | Date;
+    start_date: Date;
+    end_date: Date;
   }> = [
     {
       name: 'Incorporation',
       color: 'blue',
-      start_date: '01/01/2021',
-      end_date: '03/23/2021',
+      start_date: new Date('01/01/2021'),
+      end_date: null,
     },
     {
       name: 'Gross Assets > $50M',
       color: 'blue',
-      start_date: '03/23/2021',
-      end_date: '05/30/2021',
+      start_date: new Date('03/23/2021'),
+      end_date: null,
     },
     {
       name: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
       color: 'red',
-      start_date: '05/30/2021',
-      end_date: '12/15/2021',
+      start_date: new Date('05/30/2021'),
+      end_date: new Date('12/15/2021'),
     },
   ];
 }
--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -3,8 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 interface Event {
   name: string;
   color: string;
-  start_date: string | Date;
-  end_date: string | Date;
+  start_date: Date;
+  end_date: Date;
+}
+
+interface TimelineEvent extends Event {
+  position: string;
+  barWidth: string;
 }
 
 @Component({
@@ -14,15 +19,7 @@ interface Event {
 })
 export class TimelineComponent implements OnInit {
   @Input() events: Array<Event>;
-
-  data: Array<{
-    name: string;
-    color: string;
-    start_date: string | Date;
-    end_date: string | Date;
-    position: string;
-    barWidth: string;
-  }>;
+  data: Array<TimelineEvent>;
 
   todayDatePosition: string;
 
@@ -39,11 +36,37 @@ export class TimelineComponent implements OnInit {
     // Get position of the todays date marker
     this.todayDatePosition = this.computePosition(new Date());
 
-    this.data = this.events.map((event) => {
+    this.data = this.events.map((event, i) => {
       const evt: any = { ...event };
       evt.position = this.computePosition(evt.start_date);
-      evt.barWidth = this.computeWidth(evt.start_date, evt.end_date);
       evt.color = this.validateColor(evt.color);
+
+      /**
+       * Check if an event has an end_date
+       * If true: it will use the event end_date to compute the width of the bar
+       *
+       * Otherwise:
+       * Check if the next event is not null.
+       *
+       * If true: The start_date of the next next event will
+       * be use to compute the width of the bar of the current event.
+       *
+       * Else: will use the current date(today) to compute the width of the bar.
+       */
+      if (event.end_date) {
+        evt.barWidth = this.computeWidth(evt.start_date, evt.end_date);
+      } else {
+        if (this.events[i + 1]) {
+          const nextEvent: Event = this.events[i + 1];
+
+          evt.barWidth = this.computeWidth(
+            evt.start_date,
+            nextEvent.start_date
+          );
+        } else {
+          evt.barWidth = this.computeWidth(evt.start_date, new Date());
+        }
+      }
 
       return evt;
     });
@@ -61,11 +84,14 @@ export class TimelineComponent implements OnInit {
     const todaysDateTwoYearsAgo = new Date();
     todaysDateTwoYearsAgo.setFullYear(todaysDateTwoYearsAgo.getFullYear() - 2);
 
-    const endDate = new Date(
-      Math.max.apply(null, [
-        ...dates.filter((i) => i.end_date).map((i) => new Date(i.end_date)),
-      ])
-    );
+    const filterEndDates = [
+      ...dates.filter((i) => i.end_date).map((i) => new Date(i.end_date)),
+    ];
+
+    const endDate =
+      filterEndDates.length > 0
+        ? Math.max.apply(null, [...filterEndDates])
+        : new Date();
 
     return todaysDateTwoYearsAgo > endDate ? todaysDateTwoYearsAgo : endDate;
   }
@@ -74,7 +100,7 @@ export class TimelineComponent implements OnInit {
     const range: any =
       new Date(this.maxDate).valueOf() - new Date(this.minDate).valueOf();
     const d = new Date(date).valueOf() - new Date(this.minDate).valueOf();
-    return (d / range) * 100 + '%';
+    return Math.abs(d / range) * 100 + '%';
   }
 
   computeWidth(start: Date, end: Date) {
@@ -82,7 +108,7 @@ export class TimelineComponent implements OnInit {
       new Date(this.maxDate).valueOf() - new Date(this.minDate).valueOf();
     const d = new Date(end).valueOf() - new Date(start).valueOf();
 
-    return (d / range) * 100 + '%';
+    return Math.abs(d / range) * 100 + '%';
   }
 
   validateColor(color: string) {
